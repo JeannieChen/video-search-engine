1d890c7b75c381e1b9285a234ed5f9a9b31fb985
JeannieChen <jeannie_chen@hotmail.com>
2022-08-29 13:20:47 +0800

    changed default

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
     };
 
     componentDidMount() {
-        this.onTermSubmit('The Dodo');
+        this.onTermSubmit('NIKI Every Summertime');
     }
 
     // Call API & catch error
